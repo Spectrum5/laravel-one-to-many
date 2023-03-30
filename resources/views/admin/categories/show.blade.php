b43325120a2dadba3b644500ebc75e3c6af55242
Spectrum5 <toskusergio@gmail.com>
2023-03-30 03:03:28 +0200

    aggiunti link ai singoli post dentro la category

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -23,7 +23,9 @@
                 <ul>
                     @foreach ($category->posts as $post)
                         <li>
-                            {{ $post->title }}
+                            <a href="{{ route('admin.posts.show', $post->id) }}">
+                                {{ $post->title }}
+                            </a>
                         </li>
                     @endforeach
                 </ul>
