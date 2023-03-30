33714bf35dfb104b372f71fe7370e21aed115a62
Spectrum5 <toskusergio@gmail.com>
2023-03-30 03:26:33 +0200

    Bonus seeder

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Seeder;
 
 // Model
 use App\Models\Post;
+use App\Models\Category;
 
 // Helpers
 use Faker\Generator as Faker;
@@ -22,14 +23,20 @@ class PostSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for ($i=0; $i < 10; $i++) {
-            $title =$faker->unique()->sentence(4);
+        for ($i=0; $i < 20; $i++) {
+            $title =$faker->unique()->sentence(4); // Crea frasi da più o meno 4 parole
+
+            $categoryId = null;
+            // if (rand(0, 1) == 1) {
+            if (rand(0,1)) {
+                $categoryId = Category::inRandomOrder()->first()->id;
+            }
 
             Post::create([
                 'title' => $faker->sentence(4),
                 'slug' => Str::slug($title),
                 'content' => $faker->paragraph(),
-                // 'category_id' => .
+                'category_id' => $categoryId
             ]);
         }
     }
