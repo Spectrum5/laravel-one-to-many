401b30fa21eee976d2b3fb289e83aa52816a1e5d
Spectrum5 <toskusergio@gmail.com>
2023-03-30 02:31:00 +0200

    aggiunto show category

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -48,7 +48,7 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        //
+        return view('admin.categories.show', compact('category'));
     }
 
     /**
--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts.admin')
 
 @section('content')
-    
+    <div class="row justify-content-center md-4">
+        <div class="col">
+
+            {{-- Aggiungo partials errors --}}
+            @include('partials.success')
+
+            <h1>
+                {{ $category->name }}
+            </h1>
+
+            <h6>
+                Slug: {{ $category->slug }}
+            </h6>
+
+        </div>
+    </div>
 @endsection
