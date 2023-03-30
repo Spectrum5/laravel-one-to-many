8f0820fb06317ca59d3e73b52a91e29e13e8a1a7
Spectrum5 <toskusergio@gmail.com>
2023-03-30 02:58:13 +0200

    fix Category

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -15,6 +15,23 @@
                 Slug: {{ $category->slug }}
             </h6>
 
+            <h2>
+                Articoli associati ({{ $category->posts()->count() }})
+            </h2>
+
+            @if ($category->posts)
+                <ul>
+                    @foreach ($category->posts as $post)
+                        <li>
+                            {{ $post->title }}
+                        </li>
+                    @endforeach
+                </ul>
+            @else
+                <h3>
+                    Nessun articolo associato
+                </h3>
+            @endif
         </div>
     </div>
 @endsection
